f611983181a7c3aa17ff108dd5006074f6a26199
Lanford Gabriel Murillo <holtlanf999@hotmail.com>
2015-05-27 12:15:05 -0600

    weatherApp avance

--- weatherapp/js/main.js
+++ weatherapp/js/main.js
@@ -1,37 +1,59 @@
+// app starts...
+
 var weatherApp = ( function( window ) {
 
-	city = 0;
-	country = 0;
+	// ajax request starts...
 
 	var request = new XMLHttpRequest();
-	request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q='+ city + country, true);
+	request.open( 'GET', 'http://api.openweathermap.org/data/2.5/weather?q=', true );
 
 	request.onload = function(){
-		if(this.status >= 200 && this.status < 400){
-			var data = JSON.parse(this.response);
+		if( this.status >= 200 && this.status < 400 ){
+			var data = JSON.parse( this.response );
 			console.log( 'conection stablished' );
 			console.log( this.response );
+
 		} else {
 			alert('server reached, but it returned an error');
 		}
 	} ;
-
 	request.onerror = function(){
-		console.log('connection error');
+		console.log( 'connection error' );
 	};
 
 	request.send();	
 
+	// ajax request ends...
+
+	// Revealing module start...
+
 	function getWeather( city, country ) {
+		this.city = city;
+		this.country = country;
+
 		city = document.getElementById( 'type-city' );
+
 		country = document.getElementById( 'type-country' );
-		console.log( city.value );
-		console.log( country.value );
+
+		cityInfo : city
+		coutryInfo : country
+
+		console.log( 'ciudad: ' + city.value );
+		console.log( 'pais: ' + country.value );
 	}
 
 	return {
-		giveClima : getWeather
+		giveClima : getWeather,
+		// cityInfo,
+		// coutryInfo
 	}
 
+	console.log( giveClima.cityInfo.value );
+	console.log( giveClima.coutryInfo.value );
+
+	// Revealing module ends...
+
 } )( window );
+
+// app ends...
 
