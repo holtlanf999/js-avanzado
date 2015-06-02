049edee1c5807296e0793e3248827ac11a721b67
Lanford Gabriel Murillo <holtlanf999@hotmail.com>
2015-06-01 18:41:45 -0600

    cambios menores

--- weatherapp/js/main.js
+++ weatherapp/js/main.js
@@ -1,29 +1,38 @@
-var weatherApp = ( function( window ) {
+var weatherApp = ( function( window, undefined ) {
+	city = document.getElementById( 'type-city' );
+	country = document.getElementById( 'type-country' );
+	compUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+	displayWeather = document.getElementById( 'display-weather' );
 
-	function getWeather( city, country ) {
-		city = document.getElementById( 'type-city' );
-		country = document.getElementById( 'type-country' );
-		temp = 0;
-		humidity = 0;	
+	var pideCLima = function(){
+		var request = new XMLHttpRequest();
+		request.open( 'GET', compUrl + city.value + ',' + country.value, true );
+		
+		request.onload = function(){
+			if( this.status >= 200 && this.status < 400 ){
+				var data = JSON.parse( this.response );
+				console.log( 'conection stablished' );
+				console.log( this.response );
+			} else {
+				console.log( 'server reached, error returned' );
+			}
+		};
+		request.onerror = function(){
+			console.log( 'connection error' );
+		};
+			request.send();
+	};
+	return{
+		giveC : pideCLima
+	}
 
-		console.log( 'ciudad: ' + city.value );
-		console.log( 'pais: ' + country.value );
-		printClima = document.getElementById( 'display-weather' ).innerHTML = 
+	function getWeather(){ 
+		this.printClima = displayWeather.innerHTML = 
 			'<p>' + 'city: ' + city.value + '</p>' + 
 			'<p>' + 'country: ' + country.value + '</p>' +
-			'<p>'+ 'Temperature: ' + temp.value + '</p>' +
-			'<p>'+ 'Humidity: ' + humidity.value + '</p>'	;
-	}
+			'<p>' + 'temperature: ' + data.main.temp + '</p>'
 
-	return {
-		getC : getWeather
+			;
 	}
-
-	requestC = function(){
-		climaData = xmlhttp.open("GET","api.openweathermap.org/data/2.5/weather?q=London,uk",true);
-		document.print( climaData );
-	}
-
-	requestC();
 
 } )( window );
