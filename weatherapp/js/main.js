156240dce29ee5ebc6a87987e0950080a391a549
Lanford Gabriel Murillo <holtlanf999@hotmail.com>
2015-06-05 13:17:42 -0600

    creacion de factory pattern para imprimir la info

--- weatherapp/js/main.js
+++ weatherapp/js/main.js
@@ -1,39 +1,90 @@
 var weatherApp = ( function( window, undefined ) {
+
 	city = document.getElementById( 'type-city' );
 	country = document.getElementById( 'type-country' );
 	compUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
 	displayWeather = document.getElementById( 'display-weather' );
 
-	var pideClima = function(){
+	var getWeather = function(){
+
 		var request = new XMLHttpRequest();
 		request.open( 'GET', compUrl + city.value + ',' + country.value, true );
 		
 		request.onload = function(){
+
 			if( this.status >= 200 && this.status < 400 ){
 				var data = JSON.parse( this.response );
-				// console.log( 'conection stablished' );
 				console.log( this.response );
-				console.log( this );
 				console.log( data.weather[0].description );
 			} else {
 				console.log( 'server reached, error returned' );
 			}
+
 		};
+
 		request.onerror = function(){
 			console.log( 'connection error' );
 		};
 			request.send();
 	};
+
 	return{
-		giveC : pideClima,
+		giveC : getWeather,
 	}
 
-	function printClima(){ 
-		this.printClima = displayWeather.innerHTML = 
-			'<p>' + 'city: ' + city.value + '</p>' + 
-			'<p>' + 'country: ' + country.value + '</p>' +
-			'<p>' + 'temperature: ' + data.main.temp + '</p>';
+	function printWeather(){ 
+
+		var city =	data.name;
+		var country = data.sys.country;
+		var temp =	data.main.temp;
+		var weatherType = data.weather[0].description;
+		var icon =	data.weather[0].icon;
+		
+		var sunny = function(){
+			this.icon =	icon;
+		}
+
+		var rainy = function(){
+			this.icon =	icon;
+		}
+
+		var stormy = function(){
+			this.icon =	icon;
+		}
+
+		var cloudy = function(){
+			this.icon =	icon;
+		}
+
+
+		this.createWeatherBox = function (type) {
+
+			var weather;
+
+			if( type === "sunny" ){
+				weather = new sunnyWeather();
+			} else if( type === "rainy" ){
+				weather = new rainyWeather();
+			} else if( type === "stormy" ){
+				weather = new stormyWeather();
+			} else if( type === "cloudy" ){
+				weather = new cloudyWeather();
+			}
+
+			weather.type = type;
+
+			weather.print = function(){
+
+				displayWeather.innerHTML =
+					'<p>' + 'city: ' + city.value + '</p>' + 
+					'<p>' + 'country: ' + country.value + '</p>' +
+					'<p>' + 'temperature: ' + temp.value + '</p>' +
+					'<p>' + 'weather: ' + weatherType.value + '</p>';
+
+			}
+
+		}
+
 	}
 	
-
 } )( window );
